22a3bca02992ad3306155ef0e771b190fc952219
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-24 12:51:40 +0300

    created model

---
+++ app/src/test/java/com/example/questify/QuestModelTest.kt
@@ -0,0 +1,20 @@
+package com.example.questify
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@Config(sdk = [33])
+@RunWith(AndroidJUnit4::class)
+class QuestModelTest {
+
+    private val quest: QuestModel = QuestModel()
+
+    @Test
+    fun checkQuestExists() {
+        assertNotNull(quest)
+    }
+
+}
