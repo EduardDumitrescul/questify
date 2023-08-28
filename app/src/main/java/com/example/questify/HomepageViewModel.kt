86bee6fb7d1ce8a8a097d0a23f798d0d967e2df0
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-28 08:06:00 +0300

    created viewModel

---
+++ app/src/main/java/com/example/questify/HomepageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.questify
+
+import androidx.lifecycle.ViewModel
+import com.example.questify.data.QuestRepository
+
+class HomepageViewModel(val dataSource: QuestRepository): ViewModel() {
+}
---
+++ app/src/test/java/com/example/questify/HomepageViewModelTest.kt
@@ -0,0 +1,29 @@
+package com.example.questify
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.questify.data.QuestDataSourceLocal
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@Config(sdk = [33])
+@RunWith(AndroidJUnit4::class)
+class HomepageViewModelTest {
+    private val dataSource = QuestDataSourceLocal()
+    private val viewModel = HomepageViewModel(dataSource)
+
+    init {
+        mutableListOf(
+            QuestModel(name = "quest 1"),
+            QuestModel(name = "quest 2"),
+            QuestModel(name = "quest 3")
+        ).onEach {
+            dataSource.addQuest(it)
+        }
+    }
+    @Test
+    fun init() {
+        assertNotNull(viewModel)
+    }
+}
