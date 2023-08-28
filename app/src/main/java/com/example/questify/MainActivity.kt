f1f7ce762310befc6ce2f166fa2f0482f52fe499
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-28 15:20:13 +0300

    added Hilt for di

--- app/src/main/java/com/example/questify/MainActivity.kt
+++ app/src/main/java/com/example/questify/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.questify
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.questify.ui.homepage.HomepageScreen
 import com.example.questify.ui.theme.QuestifyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("DASDAs", "ASDASDA")
         setContent {
             QuestifyTheme {
                 HomepageScreen()
--- app/src/main/java/com/example/questify/ui/homepage/HomepageViewModel.kt
+++ app/src/main/java/com/example/questify/ui/homepage/HomepageViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.questify.ui.homepage
 
 import androidx.lifecycle.ViewModel
-import com.example.questify.data.QuestDataSourceLocal
 import com.example.questify.data.QuestRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomepageViewModel(dataSource: QuestRepository = QuestDataSourceLocal()): ViewModel() {
+@HiltViewModel
+class HomepageViewModel
+@Inject constructor(dataSource: QuestRepository):
+    ViewModel() {
 
     val quests = dataSource.getQuests()
 }
