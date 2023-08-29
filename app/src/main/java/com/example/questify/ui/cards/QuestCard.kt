5e42c63a1c1d175fc51604166cb656c2bef08fa8
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-29 13:10:51 +0300

    extends on user click

--- app/src/main/java/com/example/questify/ui/cards/QuestCard.kt
+++ app/src/main/java/com/example/questify/ui/cards/QuestCard.kt
@@ -1,22 +1,83 @@
 package com.example.questify.ui.cards
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun QuestCard(
     modifier: Modifier = Modifier,
     questName: String = "",
 ) {
-    ElevatedCard(
-        modifier = modifier.testTag("Quest Card")
-    ) {
-        Text(
-            questName,
-            modifier = Modifier.testTag("Quest Name")
+    var state by remember {
+        mutableStateOf(
+            QuestCardState(
+                questName = questName,
+                extended = false,
+            )
         )
     }
+
+    state.onClick = {
+        state = state.copy(
+            extended = !state.extended,
+            questName = "sad"
+        )
+    }
+
+    QuestCardStateless(
+        modifier = modifier,
+        state = state
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QuestCardStateless(
+    modifier: Modifier = Modifier,
+    state: QuestCardState = QuestCardState(),
+) {
+    ElevatedCard(
+        modifier = modifier.testTag("Quest Card"),
+        onClick = state.onClick,
+    ) {
+        Column() {
+            Text(
+                text = state.questName,
+                modifier = Modifier.testTag("Quest Name")
+            )
+
+            if(state.extended == true) {
+                Column(
+                    modifier = Modifier.testTag("body")
+                ) {
+                    Spacer(Modifier.height(20.dp))
+                }
+            }
+        }
+    }
+}
+
+data class QuestCardState(
+    var questName: String = "",
+    var extended: Boolean = false,
+    var onClick: () -> Unit = {}
+)
+
+@Preview(showBackground = true)
+@Composable
+fun QuestCardPreview() {
+    QuestCard(questName = "Quest Name")
 }
--- app/src/test/java/com/example/questify/ui/cards/QuestCardTest.kt
+++ app/src/test/java/com/example/questify/ui/cards/QuestCardTest.kt
@@ -3,12 +3,15 @@ package com.example.questify.ui.cards
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToString
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.questify.ui.theme.QuestifyTheme
 import org.junit.Before
-import org.junit.runner.RunWith
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
 @Config(sdk = [33])
@@ -30,6 +33,19 @@ class QuestCardTest {
 
     @Test
     fun questName() {
-        composeTestRule.onNodeWithTag("Quest Name").assertTextEquals("Quest Name")
+        composeTestRule
+            .onNodeWithTag(testTag = "Quest Name", useUnmergedTree = true)
+            .assertTextEquals("Quest Name")
+    }
+
+    @Test
+    fun performClick_verifyExtended() {
+        composeTestRule
+            .onNodeWithTag("Quest Card", useUnmergedTree = true)
+            .performClick()
+        println(composeTestRule.onRoot(useUnmergedTree = true).printToString())
+//        composeTestRule
+//            .onNodeWithTag("body", useUnmergedTree = true)
+//            .assertIsDisplayed()
     }
 }
