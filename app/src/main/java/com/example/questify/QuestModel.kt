fee9c94d74f2117a834750b5ef1a0129005abc19
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-30 07:49:43 +0300

    added more attributes

--- app/src/main/java/com/example/questify/QuestModel.kt
+++ app/src/main/java/com/example/questify/QuestModel.kt
@@ -1,12 +1,18 @@
 package com.example.questify
 
+import java.time.LocalDate
 import java.util.UUID
 
 
-data class QuestModel(
+data class QuestModel (
     val id: UUID = UUID.randomUUID(),
     var name: String = "Base Quest",
-    var description: String = ""
+    var description: String = "",
+    var startDate: LocalDate = LocalDate.now(),
+    val deadlineDate: LocalDate? = null,
+    var endDate: LocalDate? = null,
+    val targetReps: Int = 20,
+    var currentReps: Int = 0
 ) {
 
 }
--- app/src/test/java/com/example/questify/QuestModelTest.kt
+++ app/src/test/java/com/example/questify/QuestModelTest.kt
@@ -3,9 +3,11 @@ package com.example.questify
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
+import java.time.LocalDate
 
 @Config(sdk = [33])
 @RunWith(AndroidJUnit4::class)
@@ -23,6 +25,12 @@ class QuestModelTest {
         assertNotNull(quest.id)
         assertEquals(quest.name, "Base Quest")
         assertEquals(quest.description, "")
+        val date: LocalDate = LocalDate.now()
+        assertEquals(quest.startDate, date)
+        assertNull(quest.deadlineDate)
+        assertNull(quest.endDate)
+        assertEquals(quest.targetReps, 20)
+        assertEquals(quest.currentReps, 0)
     }
 
     @Test
