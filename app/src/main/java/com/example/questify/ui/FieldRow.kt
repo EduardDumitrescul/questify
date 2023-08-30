61cbe231d238cd36e25afcc367c4748fb40ee82d
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-30 20:57:13 +0300

    added fields values

--- app/src/main/java/com/example/questify/ui/FieldRow.kt
+++ app/src/main/java/com/example/questify/ui/FieldRow.kt
@@ -2,18 +2,19 @@ package com.example.questify.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FieldRow(
     modifier: Modifier = Modifier,
-    contentBegin: @Composable() () -> Unit = {},
-    contentEnd: @Composable() () -> Unit = {}
+    contentBegin: @Composable () -> Unit = {},
+    contentEnd: @Composable () -> Unit = {}
 ) {
     Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         contentBegin()
         contentEnd()
