e900359a34a09654a28bf446914aa5f205f5addd
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-08-30 10:35:06 +0300

    added title

--- app/src/main/java/com/example/questify/ui/homepage/AddQuestBottomSheet.kt
+++ app/src/main/java/com/example/questify/ui/homepage/AddQuestBottomSheet.kt
@@ -1,11 +1,13 @@
 package com.example.questify.ui.homepage
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 
@@ -19,6 +21,11 @@ fun AddQuestBottomSheet(
         modifier = modifier.testTag("Add Quest Bottom Sheet"),
         onDismissRequest = onDismissRequest
     ) {
-    Spacer(modifier = Modifier.height(64.dp))
+        Column {
+            Text(text = "New Quest")
+
+            Spacer(modifier = Modifier.height(64.dp))
+        }
+
     }
 }
---
+++ app/src/test/java/com/example/questify/ui/homepage/AddQuestBottomSheetTest.kt
@@ -0,0 +1,32 @@
+package com.example.questify.ui.homepage
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@Config(sdk = [33])
+@RunWith(AndroidJUnit4::class)
+class AddQuestBottomSheetTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun init() {
+        composeTestRule.setContent {
+            AddQuestBottomSheet(onDismissRequest = { })
+        }
+    }
+
+    @Test
+    fun verifyTitleDisplayed() {
+        composeTestRule
+            .onNodeWithText("New Quest")
+            .assertExists()
+    }
+}
