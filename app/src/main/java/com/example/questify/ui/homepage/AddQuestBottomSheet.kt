8a03169f34e66a9660ec34b3ee78ef2f1145db0c
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-09-02 08:55:36 +0300

    refactor - added stateless composable

--- app/src/main/java/com/example/questify/ui/homepage/AddQuestBottomSheet.kt
+++ app/src/main/java/com/example/questify/ui/homepage/AddQuestBottomSheet.kt
@@ -14,31 +14,50 @@ import com.example.questify.QuestModel
 import com.example.questify.ui.FieldRow
 import java.time.LocalDate
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddQuestBottomSheet(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val quest by remember {
+    val state by remember {
         mutableStateOf(
-            QuestModel()
+            AddQuestBottomSheetState(
+                onDismissRequest = onDismissRequest,
+            )
         )
     }
+
+    AddQuestBottomSheetStateless(
+        state = state,
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddQuestBottomSheetStateless(
+    state: AddQuestBottomSheetState,
+    modifier: Modifier = Modifier,
+) {
     ModalBottomSheet(
         modifier = modifier.testTag("Add Quest Bottom Sheet"),
-        onDismissRequest = onDismissRequest
+        onDismissRequest = state.onDismissRequest
     ) {
         Column {
             Text(text = "New Quest")
 
-            NameFieldRow(name = quest.name)
-            DescriptionFieldRow(description = quest.description)
-            TargetFieldRow(target = quest.targetReps)
-            DeadlineFieldRow(deadline = quest.deadlineDate)
+            NameFieldRow(name = state.quest.name)
+            DescriptionFieldRow(description = state.quest.description)
+            TargetFieldRow(target = state.quest.targetReps)
+            DeadlineFieldRow(deadline = state.quest.deadlineDate)
         }
     }
 }
+
+data class AddQuestBottomSheetState(
+    val onDismissRequest: () -> Unit = {},
+    val quest: QuestModel = QuestModel(),
+)
 
 @Composable
 fun NameFieldRow(
