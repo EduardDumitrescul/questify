d4b863a33ec1a4cea1a8f3208e074b3c253e198d
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-09-03 10:56:21 +0300

    refactored state

--- app/src/main/java/com/example/questify/ui/homepage/HomepageScreen.kt
+++ app/src/main/java/com/example/questify/ui/homepage/HomepageScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -41,7 +40,6 @@ fun HomepageScreen(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomepageScreenStateless(
     modifier: Modifier = Modifier,
