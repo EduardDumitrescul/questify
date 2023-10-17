9816f102b94d5c954bd178477ae545414728e811
Eduard-Valentin Dumitrescul <dumitrescul.eduard@gmail.com>
2023-10-17 19:04:26 +0300

    added app name as headline

--- app/src/main/java/com/example/questify/ui/homepage/HomepageScreen.kt
+++ app/src/main/java/com/example/questify/ui/homepage/HomepageScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
@@ -73,6 +75,13 @@ fun HomepageScreenStateless(
             modifier =  modifier
                 .padding(it),
         ) {
+            item {
+                Text(
+                    text = "Questify",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(48.dp)
+                )
+            }
             state.quests?.let {
                 items(it) { quest ->
                     QuestCard(
